2fe41d2ad96f8c1c4a0b6f1986b29e320253bcc2
Sneha Goddu <s.goddu@wustl.edu>
2022-03-28 21:12:43 -0500

    open written (still need to test)

--- secure_shmem.c
+++ secure_shmem.c
@@ -1,72 +1,74 @@
 
 
 //TODO: figure out how to properly use a mode_t data type
-int assemble_mode (enum access)
-{
+int assemble_mode (enum access){
+
     int mem_region_mode;
-    
-    if (access == READ)
-    {
+
+    if (access == READ){
         mem_region_mode = O_RDONLY;
     }
-    else if (access == WRITE)
-    {
+    else if (access == WRITE){
         mem_region_mode = O_WRONLY;
     }
-    else if (access == BOTH)
-    {
+    else if (access == BOTH){
         mem_region_mode = O_RDWR;
     }
-   
+
     return mem_region_mode;
     //TODO: figure out how to return an error value from this
 
 }
 
 
-int open_shared_mem (const char *name, enum create_or_join, enum access, off_t size)
-{
+void *open_shared_mem (const char *name, enum create_or_join, enum access, off_t size){
 
     //shm_open()
     int access_level = assemble_mode(access);
     int shm_fd;
-    
-    if (create_or_join == CREATE_REGION)
-    {
+
+    //if user is creating region
+    if (create_or_join == CREATE_REGION){
+
+        //create region
         shm_fd = shm_open(name, access_level | O_CREAT, S_IRWXU);
-        if (shm_fd < 0)
-        {
-            printf("there was an error creating a shared memory file descriptor\n");
-            return shm_fd;
+        if (shm_fd < 0){
+            printf("error creating a shared memory file descriptor\n");
+            return NULL;
         }
 
-        //if creating the memory region for the first time, size the region with ftruncate()
+        //resize region
         int ftruncate_ret_val = ftruncate(shm_fd, size);
+        if (ftruncate_ret_val < 0){
+            printf("error with ftruncate()\n");
+            return NULL;
+        }
+    }
 
-        //error handling for ftruncate()
-        if (ftruncate_ret_val < 0)
-        {
-            printf("there was an error with ftruncate()\n");
-            return -1;
-        }
-    }
-    else if (create_or_join == JOIN_REGION)
-    {
+    //if user is joining region
+    else if (create_or_join == JOIN_REGION){
+
+        //open region
         shm_fd = shm_open(name, access_level, S_IRWXU);
-        if (shm_fd < 0)
-        {
-            printf("there was an error creating a shared memory file descriptor\n");
-            return shm_fd;
+        if (shm_fd < 0){
+            printf("error creating a shared memory file descriptor\n");
+            return NULL;
         }
     }
-    else
-    {
+
+    else{
         errno = EINVAL;    
     }
 
+    //map region to address space
+    void* shm_addr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if(shm_addr == -1){
+        printf("error mapping to address space\n");
+        return NULL;
+    }
 
-    //mmap()
-    char* shm_addr = mmap(NULL, size, PROT_READ|PROT_WRITE, 
+    //TODO: update the data structure
 
+    return shm_addr;
     
 }
